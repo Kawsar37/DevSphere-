593e1b73403f5ca97bad36cf275f8e4930fa490d
Kawsar37 <kawsarali750@gmail.com>
2026-09-03 22:41:37 +0600

    fix(backend): configure Google DNS servers for resilient MongoDB Atlas SRV resolution on Windows

--- backend/server.ts
+++ backend/server.ts
@@ -1,3 +1,7 @@
+import dns from 'dns';
+
+dns.setServers(['8.8.8.8', '8.8.4.4']);
+
 import app from "./src/app.js";
 import { connectDB } from "./src/config/db.js";
 import { ENV } from "./src/config/env.js";
